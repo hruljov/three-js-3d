8732b4699f0f0f0fb2a7362f868198e1c05bfeb1
Fedor Myznikov <ee.post@yahoo.com>
2018-09-17 15:24:01 +0300

    lessons 11-14
    added fog and light

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
-//creating function to initialize environment
-
+/* creating function to initialize environment */
 function init() {
 
-    //setting up scene
-    var scene = new THREE.Scene();
+    var scene = new THREE.Scene();                                                                          //setting up scene
 
-    //calling getBox function
-    var box = getBox(1, 1, 1);
-    //calling getPlabe function
-    var plane = getPlane(4);
+    /* setting up fog */
+    var enableFog = false;                                                                                  //fog trigger
+    if (enableFog) {
+        scene.fog = new THREE.FogExp2(0xffffff, 0.2);                                                       //fog function
+    }
+
+    var box = getBox(1, 1, 1);                                                                              //calling getBox function
+    var plane = getPlane(20);                                                                               //calling getPlabe function
+    var pointLight = getPointLight(1);
+    var sphere = getSphere(0.05);
+
+    plane.name = 'plane-1';                                                                                 //setting plane name
+
 
     //setting up "box" position
     box.position.y = box.geometry.parameters.height/2;
     //rotating the plane. Calling "Math" module
     plane.rotation.x = Math.PI/2;
+    pointLight.position.y = 2;
 
-    //adding "box" object to the scene
-    scene.add(box);
-    //adding "plane" object to the scene
-    scene.add(plane);
-
+    scene.add(box);                                                                                         //adding box object to the scene
+    scene.add(plane);                                                                                       //adding plane object to the scene
+    scene.add(pointLight);                                                                                  //adding point light object to the scene
+    pointLight.add(sphere);                                                                                 //adding light bulb object to the scene
 
     //setting up camera
     var camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 1000);
@@ -35,6 +42,8 @@ function init() {
     var renderer = new THREE.WebGLRenderer();
     //setting up render resolution
     renderer.setSize(window.innerWidth, window.innerHeight);
+    //setting background
+    renderer.setClearColor('rgb(120, 120, 120)');
 
     /*
     DOM (Document Object Model) – объектная модель, используемая для XML/HTML-документов.
@@ -49,41 +58,59 @@ function init() {
 
 }
 
+
+/* creating box object */
 function getBox(w, h, d) {
 
-    //setting up box geometry
-    var geometry = new THREE.BoxGeometry(w, h, d);
-
-    //setting up the material
-    var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-
-    //creating a mesh object from the geometry and a material
-    var mesh = new THREE.Mesh(geometry, material);
+    var geometry = new THREE.BoxGeometry(w, h, d);                                                              //setting up box geometry
+    var material = new THREE.MeshPhongMaterial({color: 'rgb(120, 120, 120)'});                                  //setting up the material
+    var mesh = new THREE.Mesh(geometry, material);                                                              //creating a mesh object from the geometry and a material
 
     return mesh;
+
 }
 
+
+/* creating sphere object */
+function getSphere(size) {
+
+    var geometry = new THREE.SphereGeometry(size, 24, 24);                                                              //setting up sphere geometry
+    var material = new THREE.MeshBasicMaterial({color: 'rgb(255, 255, 255)'});                                  //setting up the material
+    var mesh = new THREE.Mesh(geometry, material);                                                              //creating a mesh object from the geometry and a material
+
+    return mesh;
+
+}
+
+
+/* creating plane object */
 function getPlane(size) {
 
-    //setting up box geometry
-    var geometry = new THREE.PlaneGeometry(size, size);
-
-    //setting up the material
-    var material = new THREE.MeshBasicMaterial({color: 0xff0000, side: THREE.DoubleSide});
-
-    //creating a mesh object from the geometry and a material
-    var mesh = new THREE.Mesh(geometry, material);
+    var geometry = new THREE.PlaneGeometry(size, size);                                                         //setting up box geometry
+    var material = new THREE.MeshPhongMaterial({color: 'rgb(120, 120, 120)', side: THREE.DoubleSide});          //setting up the material
+    var mesh = new THREE.Mesh(geometry, material);                                                              //creating a mesh object from the geometry and a material
 
     return mesh;
+
 }
 
+
+/* creating point light object */
+function getPointLight(intensity) {
+    
+    var light = new THREE.PointLight(0xffffff, intensity);                                                      //setting up point light using color and intensity
+    
+    return light;
+
+}
+
+
+/* setting up frame updates */
 function update(renderer, scene, camera) {
     
-    //calling render method using the "scene" and "camera" as arguments
-    renderer.render(scene, camera);
+    renderer.render(scene, camera);                                                                             //calling render method using the "scene" and "camera" as arguments
+    requestAnimationFrame(function() {update(renderer, scene, camera);})                                        //creating recursive function to continiously update scene
 
-    //creating recursive function to continiously update scene
-    requestAnimationFrame(function() {update(renderer, scene, camera);})
 }
 
 var scene = init();
