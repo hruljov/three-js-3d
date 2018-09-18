8e0b367b94f05f7c66482d03ef582f2702538f22
Fedor Myznikov <ee.post@yahoo.com>
2018-09-18 18:47:33 +0300

    lessons 15-17
    GUI
    Camera controls
    Shadows

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 /* creating function to initialize environment */
 function init() {
 
-    var scene = new THREE.Scene();                                                                              //setting up scene
+    var scene = new THREE.Scene();                                                                              //initialising scene
+    var gui = new dat.GUI();                                                                                    //initialising dat.gui
 
     /* setting up fog */
     var enableFog = false;                                                                                      //fog trigger
@@ -15,12 +16,17 @@ function init() {
     var pointLight = getPointLight(1);                                                                          //calling getPointLight function
     var sphere = getSphere(0.05);                                                                               //calling getSphere function
 
-    plane.name = 'plane-1';                                                                                     //setting plane name
+    plane.name = 'plane-1';                                                                                     //setting up plane name
 
     /* transforming objects */
     box.position.y = box.geometry.parameters.height/2;                                                          //setting up box position
     plane.rotation.x = Math.PI/2;                                                                               //rotating plane using "Math" module
     pointLight.position.y = 2;                                                                                  //setting up point light position
+    pointLight.intensity = 2;                                                                                   //setting up pointlight intensity
+
+    /* creating interface */
+    gui.add(pointLight, 'intensity', 0, 10);                                                                    //creating user interface controller
+    gui.add(pointLight.position, 'y', 0, 5);
 
     /* adding objects to the scene */
     scene.add(box);                                                                                             //adding box object to the scene
@@ -37,6 +43,7 @@ function init() {
     
     /* setting up renderer */
     var renderer = new THREE.WebGLRenderer();                                                                   //creating renderer
+    renderer.shadowMap.enabled = true;                                                                          //enabling shadows
     renderer.setSize(window.innerWidth, window.innerHeight);                                                    //setting up render resolution
     renderer.setClearColor('rgb(120, 120, 120)');                                                               //setting background
 
@@ -46,7 +53,9 @@ function init() {
     */
     document.getElementById('webgl').appendChild(renderer.domElement);
     
-    update(renderer, scene, camera);                                                                            //calling "update" function
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);                                        //assigning controls
+
+    update(renderer, scene, camera, controls);                                                                  //calling "update" function
 
     return scene;
 
@@ -59,6 +68,7 @@ function getBox(w, h, d) {
     var geometry = new THREE.BoxGeometry(w, h, d);                                                              //setting up box geometry
     var material = new THREE.MeshPhongMaterial({color: 'rgb(120, 120, 120)'});                                  //setting up the material
     var mesh = new THREE.Mesh(geometry, material);                                                              //creating a mesh object from the geometry and a material
+    mesh.castShadow = true;                                                                                     //setting up the box to cast shadows
 
     return mesh;
 
@@ -83,6 +93,7 @@ function getPlane(size) {
     var geometry = new THREE.PlaneGeometry(size, size);                                                         //setting up box geometry
     var material = new THREE.MeshPhongMaterial({color: 'rgb(120, 120, 120)', side: THREE.DoubleSide});          //setting up the material
     var mesh = new THREE.Mesh(geometry, material);                                                              //creating a mesh object from the geometry and a material
+    mesh.receiveShadow = true;                                                                                  //setting up the plane to receive shadows
 
     return mesh;
 
@@ -93,17 +104,19 @@ function getPlane(size) {
 function getPointLight(intensity) {
     
     var light = new THREE.PointLight(0xffffff, intensity);                                                      //setting up point light using color and intensity
-    
+    light.castShadow = true;                                                                                    //setting up the light to cast shadows
+
     return light;
 
 }
 
 
 /* setting up frame updates */
-function update(renderer, scene, camera) {
+function update(renderer, scene, camera, controls) {
     
     renderer.render(scene, camera);                                                                             //calling render method using the "scene" and "camera" as arguments
-    requestAnimationFrame(function() {update(renderer, scene, camera);})                                        //creating recursive function to continiously update scene
+    controls.update();
+    requestAnimationFrame(function() {update(renderer, scene, camera, controls);})                              //creating recursive function to continiously update scene
 
 }
 
